9ec026f670f5d017b964df391752f2e4d5624b12
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-06-15 21:07:47 +0200

    Fieldset component imported

--- src/App.js
+++ src/App.js
@@ -5,30 +5,35 @@ import Legend from "./Legend";
 import Select from "./Select";
 import Resualts from "./Resualts";
 import Buttons from "./Buttons";
+import Fieldset from "./Fieldset";
 
 function App() {
   return (
     <form className="form">
-      <fieldset className="form__fieldset">
-        <Legend />
-        <Section
-          children={
-            <>
-              <Header title="Kwota w PLN" />
-              <Input />
-            </>
-          }
-        />
-        <Section
-          children={
-            <>
-              <Header title="Wybierz walutę" />
-              <Select />
-            </>
-          }
-        />
-        <Resualts />
-      </fieldset>
+      <Fieldset
+        children={
+          <>
+            <Legend />
+            <Section
+              children={
+                <>
+                  <Header title="Kwota w PLN" />
+                  <Input />
+                </>
+              }
+            />
+            <Section
+              children={
+                <>
+                  <Header title="Wybierz walutę" />
+                  <Select />
+                </>
+              }
+            />
+            <Resualts />
+          </>
+        }
+      />
       <Buttons />
     </form>
   );
