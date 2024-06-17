422eb9ef91db3ca82debce60d334b82d68222b63
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-06-17 08:48:12 +0200

    Deleted unnecessary component's files

--- src/Fieldset/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const Fieldset = ({ children }) => (
-    <fieldset className="fieldset">
-        {children}
-    </fieldset>
-);
-
-export default Fieldset;
--- src/Legend/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const Legend = () => (
-    <legend className="legend">
-        kalkulator walut
-    </legend>
-);
-
-export default Legend;
