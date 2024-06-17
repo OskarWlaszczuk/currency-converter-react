49b284cc12fbc95009cdc6d703500578fc1dc60e
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-06-17 08:45:37 +0200

    Reduced amount of component to two - Form, and Resualt. Body's of rest components moved to the Form component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import Section from "./Section";
-import Header from "./Header";
-import Input from "./Input";
-import Legend from "./Legend";
-import Select from "./Select";
-import Resualts from "./Resualts";
-import Buttons from "./Buttons";
-import Fieldset from "./Fieldset";
 import Form from "./Form";
+import Resualts from "./Resualts";
 
 function App() {
   return (
-    <Form
-      children={
-        <>
-          <Fieldset
-            children={
-              <>
-                <Legend />
-                <Section
-                  children={
-                    <>
-                      <Header title="Kwota w PLN" />
-                      <Input />
-                    </>
-                  }
-                />
-                <Section
-                  children={
-                    <>
-                      <Header title="Wybierz walutę" />
-                      <Select />
-                    </>
-                  }
-                />
-                <Resualts />
-              </>
-            }
-          />
-          <Buttons />
-        </>
-      }
-    />
+    <>
+      <Form />
+      <Resualts />
+    </>
   );
 };
 
