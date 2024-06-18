1a0023e933635d53c93bcbd215a49624d2ad98a1
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-06-18 09:54:29 +0200

    Moved curriences array to seperrated module

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { name: "Dolar amerykański", id: "$", rate: 0.24, shortcut: "$" },
+    { name: "Euro", id: "€", rate: 0.20, shortcut: "€" },
+    { name: "Funt brytyjski", id: "£", rate: 0.22, shortcut: "£" },
+    { name: "Frank szwajcarski", id: "CHF", rate: 0.23, shortcut: "CHF" },
+];
+
+export default currencies;
