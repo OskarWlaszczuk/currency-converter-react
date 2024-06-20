577de3647ca19216ed523667e94e2836ccdbc4d1
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-06-20 10:07:01 +0200

    Changed result variable on object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import { useState } from "react";
 import Form from "./Form";
-import Results from "./Results";
-import currencies from "./currencies";
+import Result from "./Result";
 
 function App() {
-  const [result, setResult] = useState();
+  const [result, setResult] = useState({});
 
-  const displayResult = (amount, currency) => {
-    const chosenCurrency = currencies.find(({ name }) => name === currency);
-
-    chosenCurrency && amount && (
-      setResult(result => result =
-        <>
-          {amount}zł = <strong className="results__result">{(chosenCurrency.rate * amount).toFixed(2)}{chosenCurrency.shortcut}</strong>
-        </>
-      )
-    );
+  const displayResult = (amount, rate, shortcut) => {
+    setResult(result => result =
+    {
+      amount,
+      rate,
+      shortcut,
+    });
   };
+
   return (
     <>
       <Form displayResult={displayResult} setResult={setResult} result={result} />
-      <Results result={result} />
+      <Result result={result} />
     </>
   );
 };
