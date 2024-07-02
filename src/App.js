83d24143dbc7d9c2d514c0623a916fa958bf19be
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-02 11:11:03 +0200

    ThemeProvider added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import Form from "./Form";
 import Result from "./Result";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  breakPoints: {
+    tabletHorizontal: 770
+  }
+};
 
 function App() {
   const [result, setResult] = useState({});
@@ -15,10 +22,10 @@ function App() {
   };
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Form displayResult={displayResult} setResult={setResult} result={result} />
       <Result result={result} />
-    </>
+    </ThemeProvider>
   );
 };
 
