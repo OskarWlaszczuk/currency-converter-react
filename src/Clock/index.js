78b01dad11a92c218a4c6f5d0c7b3d35e3c2f04b
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-02 17:54:36 +0200

    Initialization of the date variable using the custom hook useCurrentData in React

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,20 +1,12 @@
-import { useEffect, useState } from "react";
 import { Container, Paragraph } from "./styled";
+import { useCurrentData } from "./useCurrentDate";
 
 const Clock = () => {
-    const [clock, setClock] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setClock(new Date());
-        });
-        return () => clearInterval(intervalId);
-    }, []);
-
+    const date = useCurrentData()
     return (
         <Container>
             <Paragraph>
-                Dzisiaj jest {clock.toLocaleString("pl", { weekday: "long", day: "numeric", month: "long", year: "numeric" })} {clock.toLocaleTimeString("pl", { hours: "2-digit", minutes: "2-digit", seconds: "2-digit" })}
+                {date}
             </Paragraph>
         </Container>
     );
