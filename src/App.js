c19c16a3628d8847cede62936db016792485d7a0
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-03 08:24:23 +0200

    Theme provider moved to index.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import { useState } from "react";
 import Form from "./Form";
 import Result from "./Result";
-import { ThemeProvider } from "styled-components";
-
-const theme = {
-  breakPoints: {
-    tabletHorizontal: 770
-  }
-};
 
 function App() {
   const [result, setResult] = useState({});
@@ -22,10 +15,10 @@ function App() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Form displayResult={displayResult} setResult={setResult} result={result} />
       <Result result={result} />
-    </ThemeProvider>
+    </>
   );
 };
 
