9d16cbe8ae971c7d7aee6f2e6b63bf159d5da474
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-09 12:53:46 +0200

    date's functions shorted

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,13 +2,22 @@ import { Container, Paragraph } from "./styled";
 import { useCurrentData } from "./useCurrentDate";
 
 export const Clock = () => {
-    const date = useCurrentData()
-    return (
-        <Container>
-            <Paragraph>
-                Dzisiaj jest {date.toLocaleString("pl", { weekday: "long", day: "numeric", month: "long", year: "numeric" })}
-                {date.toLocaleTimeString("pl", { hours: "2-digit", minutes: "2-digit", seconds: "2-digit" })}
-            </Paragraph>
-        </Container>
-    );
+  const date = useCurrentData();
+  return (
+    <Container>
+      <Paragraph>
+        Dzisiaj jest{" "}
+        {date.toLocaleString("pl", {
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })} {date.toLocaleTimeString("pl", {
+          hours: "2-digit",
+          minutes: "2-digit",
+          seconds: "2-digit",
+        })}
+      </Paragraph>
+    </Container>
+  );
 };
