d60b86f1d799ce23e3f5db481fea8511cd801e73
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-09 12:52:08 +0200

    useExchangeApi imported and destructered

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,70 +1,96 @@
 import { useRef, useState } from "react";
-import { Fieldset, Legend, Section, Header, Input, Container, Button, SubmitButton } from "./styled";
-import currencies from "../currencies";
+import { Fieldset, Legend, Section, Header, Input, Container, Button, SubmitButton, Information } from "./styled";
 import { Clock } from "./Clock";
+import { useExchangeApi } from "../useExchangeApi";
 
-const Form = ({ displayResult, setResult }) => {
+export const Form = ({ displayResult, setResult }) => {
+    const {
+        currienciesDatas,
+        currenciesRatesDate,
+    } = useExchangeApi();
+
+    const [currency, setCurrency] = useState("CHF");
     const [amount, setAmount] = useState("");
-    const [currency, setCurrency] = useState("Euro");
     const inputRef = useRef(null);
-
+    //Przenieść logikę formularza do hooków 
+    
     const resetForm = () => {
         setAmount("");
-        setCurrency("Euro");
+        setCurrency(currienciesDatas.CHF.code);
         setResult({});
     };
 
     const onSelectChange = ({ target }) => setCurrency(target.value);
-    const onInputChange = ({ target }) => setAmount(target.value);
+    const onInputChange = ({ target }) => {
+        setAmount(target.value);
+    }
     const onFormSubmit = event => {
-        event.preventDefault();
-        inputRef.current.focus();
-        const chosenCurrency = currencies.find(({ name }) => name === currency);
-        const { rate, shortcut } = chosenCurrency
+            event.preventDefault();
 
-        displayResult(amount, rate, shortcut);
+            inputRef.current.focus();
+
+            const chosenCurrency = Object.values(currienciesDatas).find(({ code }) =>
+                code === currency);
+            const { code, value } = chosenCurrency
+
+            displayResult(amount, code, value);
+        };
+
+        return (
+            <form onSubmit={onFormSubmit}>
+                <Fieldset>
+                    {
+                        !currienciesDatas ?
+                            'Chwilę ładuję stronę' :
+                            <>
+                                <Legend>Kalkulator walut</Legend>
+                                {<Clock />}
+                                <Section>
+                                    <Header>Kwota w PLN</Header>
+                                    <Input
+                                        type="number"
+                                        ref={inputRef}
+                                        onChange={onInputChange}
+                                        value={amount}
+                                        required
+                                        min="0.1"
+                                        max="9999"
+                                        name="plnAmount"
+                                        step="any"
+                                        placeholder="Wpisz kwotę w PLN"
+                                        autoFocus
+                                    />
+                                </Section>
+                                <Section>
+                                    <Header>Wybierz walutę</Header>
+                                    <Input
+                                        as="select"
+                                        onChange={onSelectChange}
+                                        name="currencyName"
+                                        value={currency}>
+                                        {
+                                            //zmienna globalna z tablicą nazw walut
+                                            Object.keys(currienciesDatas).map((currencyName) => (
+                                                <option
+                                                    key={currencyName}
+                                                    value={currencyName}
+                                                >
+                                                    {currencyName}
+                                                </option>
+                                            ))
+                                        };
+                                    </Input>
+                                </Section>
+                                <Container>
+                                    <SubmitButton type="submit">Przelicz</SubmitButton>
+                                    <Button onClick={resetForm} type="reset">Wyczyść</Button>
+                                </Container>
+                                <Information>
+                                    Kursy walut pobierane są z ofizjalnych instytucji finansowych.<br />
+                                    Aktualne na dzień:{currenciesRatesDate}
+                                </Information>
+                            </>}
+                </Fieldset>
+            </form>
+        );
     };
-
-    return (
-        <form onSubmit={onFormSubmit}>
-            <Fieldset>
-                <Legend>Kalkulator walut</Legend>
-                {<Clock />}
-                <Section>
-                    <Header>Kwota w PLN</Header>
-                    <Input
-                        ref={inputRef}
-                        onChange={onInputChange}
-                        value={amount}
-                        required
-                        type="number"
-                        min="0.1"
-                        max="9999"
-                        name="plnAmount"
-                        step="any"
-                        placeholder="Wpisz kwotę w PLN"
-                        autoFocus
-                    />
-                </Section>
-                <Section>
-                    <Header>Wybierz walutę</Header>
-                    <Input as="select" onChange={onSelectChange} name="currencyName" value={currency}>
-                        {
-                            currencies.map(({ name, id }) => (
-                                <option key={id} value={name}>
-                                    {name}
-                                </option>
-                            ))
-                        };
-                    </Input>
-                </Section>
-                <Container>
-                    <SubmitButton type="submit">Przelicz</SubmitButton>
-                    <Button onClick={resetForm} type="reset">Wyczyść</Button>
-                </Container>
-            </Fieldset>
-        </form>
-    );
-};
-
-export default Form;
