32a9e3ea7356852c6980948a014a19e5a28da6ae
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-10 13:41:54 +0200

    Error component created, and exported

---
+++ src/Form/Error/index.js
@@ -0,0 +1,9 @@
+import { ErrorLog, Important } from "./styled"
+
+export const Error = () => (
+    <ErrorLog>
+        Sprawdź połączenie z internetem🌍<br />
+        Jeżeli problem nadal występuje to błąd leży po naszej stronie👽...<br />
+        <Important>Przepraszamy!</Important>
+    </ErrorLog>
+)
