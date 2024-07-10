f1d7f674aa3eea848b31dd0ab127211f7224e4ca
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-10 13:42:18 +0200

    Added styles to styled-components of Error component

---
+++ src/Form/Error/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ErrorLog = styled.p`
+    text-align:center;
+    font-size:larger;
+`;
+
+export const Important = styled.strong`
+    font-size:x-large;
+    font-weight:bold;
+`;
